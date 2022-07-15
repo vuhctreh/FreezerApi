05e4b6c1165d677ab456238b77d94766dd8a5700
Victor <victor.lecardonnel@hotmail.fr>
2022-07-15 15:22:20 +0100

    Initial commit

---
+++ src/test/java/com/northrow/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northrow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
